621247002af2cff7f05fff2fa7983f4c0588051a
xpfxzxc <xpfxzxc@gmail.com>
2026-02-05 13:53:29 +0800

    chore(config): 配置 session cookie 在生产环境使用 secure 属性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    session: {
+      cookie: {
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/icon',
